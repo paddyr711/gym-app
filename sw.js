0d98ad3a5d238c376ff6d18b5c0d4819fdd7396e
paddyr711 <paddylrochford@gmail.com>
2026-08-24 16:41:50 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+/* Cache-first app shell. Bump CACHE when you change any file. */
+var CACHE = 'home-gym-v1';
+var SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(SHELL); }).then(function () {
+    return self.skipWaiting();
+  }));
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.filter(function (k) { return k !== CACHE; })
+      .map(function (k) { return caches.delete(k); }));
+  }).then(function () { return self.clients.claim(); }));
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function (hit) {
+      return hit || fetch(e.request).then(function (res) {
+        var copy = res.clone();
+        caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+        return res;
+      }).catch(function () { return caches.match('./index.html'); });
+    })
+  );
+});
