e6636dd59eae7ddaffa2bd4b9c311733aed5fcc0
paddyr711 <paddylrochford@gmail.com>
2026-08-31 17:58:06 +0100

    V4 with superset button

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Cache-first app shell. Bump CACHE when you change any file. */
-var CACHE = 'home-gym-v3';
+var CACHE = 'home-gym-v4';
 var SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', function (e) {
